0e743ad70811e5da607569cd689b1bfe50bf355e
sebchi-crtl <chiemelienwobodo8@gmail.com>
2025-09-05 11:34:13 +0100

    Revise Mission component to update the mission statement and enhance content clarity, replacing placeholder text with a more impactful description of the company's goals and values.

--- app/_components/about/Mission.tsx
+++ app/_components/about/Mission.tsx
@@ -5,18 +5,16 @@ export default function Mission() {
             {/* Top Orange Line - covers approximately one-third of width */}
             <div className="absolute top-0 min-md:left-0 w-1/3 h-1 bg-[#FD9717] animate-fade-in-up hover:bg-orange-600 transition-colors duration-300"></div>
                 <div className="font-bold text-3xl lg:text-5xl xl:text-6xl 2xl:text-7xl animate-fade-in-up-delay hover:scale-105 transition-transform duration-300">
-                    Our Mission Statement
+                    The Mission Statement
                 </div>
             {/* Middle Orange Line - centered and covers approximately one-quarter of width */}
             <div className="absolute 2xl:top-20 max-md:right-0 min-md:left-1/2 transform -translate-x-1/2 w-1/4 h-1 bg-[#FD9717] animate-fade-in-up-delay-2 hover:bg-orange-600 transition-colors duration-300"></div>
             
             {/* Content can be added here if needed */}
             <div className="container mx-auto px-4 py-8 text-xl mt-6 animate-fade-in-up-delay-3 hover:scale-[1.01] transition-transform duration-300">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-
+                Build, scale, and evolve products that matter - powered by cutting edge research, design, and 
+                engineering - to deliver seamless, secure, future-proof platforms. From Attend to the next wave 
+                of tools in productivity, financial technology, entertainment, and beyond.
             </div>
         </div>
     );
